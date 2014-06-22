68db8e9e545e42450e037bbfe82258b72baf221d
imcoddy <imcoddy@gmail.com>
2014-06-22 20:17:02 +0900

    add qr code image

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,11 +8,19 @@ angular.module('myApp.controllers', []).
       $scope.name = data.name;
     });
   }).
-  controller('MyCtrl1', function ($scope, socket) {
+  controller('MyCtrl1', function ($scope, $sce, socket) {
     socket.on('send:time', function (data) {
       $scope.time = data.time;
     });
+
+    var qrCode = qrcode(3, 'M');
+    var text = 'test'; //TODO replace dynamicly later
+    text = text.replace(/^[\s\u3000]+|[\s\u3000]+$/g, '');
+    qrCode.addData(text);
+    qrCode.make();
+    var qrCodeImg = qrCode.createImgTag(4);
+    $scope.qrCodeImg = $sce.trustAsHtml(qrCodeImg);
   }).
-  controller('MyCtrl2', function ($scope) {
+    controller('MyCtrl2', function ($scope) {
     // write Ctrl here
   });
