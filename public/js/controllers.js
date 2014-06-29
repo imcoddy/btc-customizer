5ff26c45a9e48baf465311aaf94a9ca606529452
imcoddy <imcoddy@gmail.com>
2014-06-29 10:10:42 +0900

    refine ui, add contact info

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -7,19 +7,26 @@ angular.module('myApp.controllers', []).
   }).
   controller('DesignCtrl', function ($scope, $sce, socket) {
     $scope.updateQRCode = function() {
-        var qrCode = qrcode(8, 'M');
-        var text = $scope.qrCodeText;
-        text = typeof text === 'undefined' ? '' : text;
-        qrCode.addData(text);
-        qrCode.make();
-        var qrCodeImg = qrCode.createTableTag(4, 0);
-        $scope.qrCodeImg = $sce.trustAsHtml(qrCodeImg);
-    }
+      var qrCode = qrcode(8, 'M');
+      var text = $scope.qrCodeText;
+      text = typeof text === 'undefined' ? '' : text;
+      qrCode.addData(text);
+      qrCode.make();
+      var qrCodeImg = qrCode.createTableTag(4, 0);
+      $scope.qrCodeImg = $sce.trustAsHtml(qrCodeImg);
+    };
+
+    $scope.updatePrice = function() {
+      $scope.infoPrice = $scope.infoNum * 60;
+      return $scope.infoPrice;
+    };
 
     $scope.backTextA = 'Bitcoin private key';
     $scope.backTextB = 'In Cryptography We Trust';
     $scope.backTextC = '2014';
     $scope.qrCodeText = 'QRCode Text';
+    $scope.infoNum = 1;
+    $scope.infoPrice = $scope.updatePrice();
     $scope.updateQRCode();
   }).
     controller('AboutCtrl', function ($scope) {
